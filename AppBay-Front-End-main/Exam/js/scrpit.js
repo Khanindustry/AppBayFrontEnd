554dba6de44311f83d50fb6a8183f75b30ef5d94
Khanindustry <103564716+Khanindustry@users.noreply.github.com>
2022-04-13 13:58:29 +0400

    Add files via upload

---
+++ AppBay-Front-End-main/Exam/js/scrpit.js
@@ -0,0 +1,36 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
